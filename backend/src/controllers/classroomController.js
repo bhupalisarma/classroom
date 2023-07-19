9fb34e22cc01dad447d431ade6b39087e663d6d2
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-07-19 15:16:09 +0530

    Clinet and server working

---
+++ backend/src/controllers/classroomController.js
@@ -0,0 +1,38 @@
+// controllers/classroomController.js
+
+const Classroom = require('../models/Classroom');
+
+// Get all classrooms
+const getAllClassrooms = async (req, res) => {
+    try {
+        const classrooms = await Classroom.find();
+        res.json(classrooms);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch classrooms' });
+    }
+};
+
+// Create a classroom
+const createClassroom = async (req, res) => {
+    try {
+        const { subject, standard } = req.body;
+
+        // Create a new classroom
+        const classroom = new Classroom({
+            subject,
+            standard,
+        });
+
+        // Save the classroom to the database
+        const savedClassroom = await classroom.save();
+
+        res.json(savedClassroom);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create classroom' });
+    }
+};
+
+module.exports = {
+    getAllClassrooms,
+    createClassroom,
+};
